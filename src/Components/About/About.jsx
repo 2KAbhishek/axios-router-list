6431b0b9771f94f668a982852b26a9e8c9c6a57f
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-07 01:49:12 +0530

    Add About.jsx

---
+++ src/Components/About/About.jsx
@@ -0,0 +1,13 @@
+import "./About.css";
+export default function About() {
+  return (
+    <div className="About">
+      <h2>This is the about page.</h2>
+      <p>
+        Data fetching powered by <strong>Axios</strong>.
+        <br /> Routing powered by <strong>React Router</strong>.
+        <br /> Serving data from <strong>json-server</strong>.
+      </p>
+    </div>
+  );
+}
